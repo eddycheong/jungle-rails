df05f5217e11c3e632ea8ce804b79d70534b6a1e
eddycheong <cheong.e49@gmail.com>
2017-11-08 21:09:31 +0000

    Implemented create action for user controller, which now creates a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,25 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to products_path, notice: 'User created!'
+    else
+      render :new
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(
+        :first_name,
+        :last_name,
+        :email,
+        :password_digest
+      )
+    end
+
 end
