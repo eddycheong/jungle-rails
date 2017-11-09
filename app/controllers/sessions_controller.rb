279cc6b03cd636d5c08466f3897f0179d3416297
eddycheong <cheong.e49@gmail.com>
2017-11-09 03:16:24 +0000

    Implement logout session for login user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email])
-    
+
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to root_path
@@ -15,6 +15,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to login_path
   end
 
 end
