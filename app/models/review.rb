52b01577535ef505fbf8522c9f270001752942b4
eddycheong <cheong.e49@gmail.com>
2017-11-09 22:28:05 +0000

    Added associations between models of product, review, and user

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,7 @@
 class Review < ActiveRecord::Base
+  validates :product_id, :user_id, presence: true
+  validates :rating, inclusion: { in: (1..5) }, allow_nil: true
+
+  belongs_to :product
+  belongs_to :user
 end
