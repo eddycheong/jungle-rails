edd24fa4e9944343a994b1da60439e72d985cb2a
eddycheong <cheong.e49@gmail.com>
2017-11-09 02:49:25 +0000

    Added a dropdown for guest to login or to register

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email])
-
+    
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to root_path
