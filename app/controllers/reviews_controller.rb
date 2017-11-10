def116ca69c9df7ce7dfe1374f6498890613b44d
eddycheong <cheong.e49@gmail.com>
2017-11-10 21:01:00 +0000

    create a method 'current_user' and refactored reviews_controller to add to review users

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,11 @@
 class ReviewsController < ApplicationController
-  before_action :require_login
+  before_filter :authorize
 
   def create
     @product = Product.find(params[:product_id])
     
     @review = @product.reviews.new(review_params)
-    @review.user = User.find(session[:user_id])
+    @review.user = current_user
 
     if @review.save
       redirect_to @product, notice: 'Review Created'
@@ -17,24 +17,13 @@ class ReviewsController < ApplicationController
   def destroy
     @review = Review.find params[:id]
 
-    @product = Product.find(@review.product_id)
+    @product = Product.find @review.product_id
 
     @review.destroy
     redirect_to @product
   end
 
   private
-    def require_login
-      unless logged_in?
-        flash[:error] = "You must be logged in to access this section"
-        redirect_to login_path
-      end
-    end
-
-    def logged_in?
-      !session[:user_id].nil?
-    end
-
     def review_params
       params.require(:review).permit(
         :rating,
