486604b9e8b8c13f74cae307b6776b4faf60f2cd
eddycheong <cheong.e49@gmail.com>
2017-11-10 02:11:33 +0000

    Implemented the review form for product#show pages that allows users create reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,26 @@
 class ReviewsController < ApplicationController
   def create
-    raise "Yay, I'm here!"
+    @product = Product.find(params[:product_id])
+    
+    new_review_params = review_params
+    new_review_params[:user_id] = session[:user_id]
+
+    @review = @product.reviews.new(new_review_params)
+    @review.user = current_user
+
+    if @review.save
+      redirect_to @product, notice: 'Review Created'
+    else
+      redirect_to @product
+    end
   end
+
+  private
+    def review_params
+      params.require(:review).permit(
+        :rating,
+        :description,
+        :product_id
+      )
+    end
 end
