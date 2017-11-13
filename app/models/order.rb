69967a18cae8e4237a3d1b74581468ca862e6974
eddycheong <cheong.e49@gmail.com>
2017-11-13 21:56:38 +0000

    Added feature such that a created order will deduct quantities of products

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ActiveRecord::Base
+  after_create :deduct_products_quantity
 
   belongs_to :user
   has_many :line_items
@@ -7,4 +8,13 @@ class Order < ActiveRecord::Base
 
   validates :stripe_charge_id, presence: true
 
+  protected
+    def deduct_products_quantity
+      self.line_items.each do |line_item|
+        product = Product.find(line_item.product_id)
+
+        product.quantity -= line_item.quantity
+        product.save!
+      end
+    end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -21,13 +21,11 @@ RSpec.describe Order, type: :model do
       cart_total = (@product1.price * quantity1 + @product2.price * quantity2) * 100
 
       # 1. initialize order with necessary fields (see orders_controllers, schema and model definition for what is required)
-    
       @order = Order.new(
         total_cents: cart_total,
         stripe_charge_id: "test"
       )
       # 2. build line items on @order
-      # ...
       @order.line_items.new(
         product: @product1,
         quantity: quantity1,
@@ -44,20 +42,55 @@ RSpec.describe Order, type: :model do
 
       # 3. save! the order - ie raise an exception if it fails (not expected)
       @order.save!
-      byebug
       # 4. reload products to have their updated quantities
       @product1.reload
       @product2.reload
       # 5. use RSpec expect syntax to assert their new quantity values
-      # ...
       expect(@product1.quantity).to eq(original_quantity1-quantity1)
       expect(@product2.quantity).to eq(original_quantity2-quantity2)
     end
+
+    
     it 'does not deduct quantity from products that are not in the order' do
-      # TODO: Implement based on hints in previous test
+      # setup
+      original_quantity1 = @product1.quantity
+      original_quantity2 = @product2.quantity
       original_quantity3 = @product3.quantity
-      
+
+      quantity1 = 5
+      quantity2 = 6
+
+      cart_total = (@product1.price * quantity1 + @product2.price * quantity2) * 100
+
+      # 1. initialize order with necessary fields (see orders_controllers, schema and model definition for what is required)
+      @order = Order.new(
+        total_cents: cart_total,
+        stripe_charge_id: "test"
+      )
+      # 2. build line items on @order
+      @order.line_items.new(
+        product: @product1,
+        quantity: quantity1,
+        item_price: @product1.price,
+        total_price: @product1.price * quantity1
+      )
+
+      @order.line_items.new(
+        product: @product2,
+        quantity: quantity2,
+        item_price: @product2.price,
+        total_price: @product2.price * quantity2
+      )
+
+      # 3. save! the order - ie raise an exception if it fails (not expected)
+      @order.save!
+      # 4. reload products to have their updated quantities
+      @product1.reload
+      @product2.reload
       @product3.reload
+      # 5. use RSpec expect syntax to assert their new quantity values
+      expect(@product1.quantity).to eq(original_quantity1-quantity1)
+      expect(@product2.quantity).to eq(original_quantity2-quantity2)
       expect(@product3.quantity).to eq(original_quantity3)
     end
   end
