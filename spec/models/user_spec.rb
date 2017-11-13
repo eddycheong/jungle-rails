4bf4b69b38d73c2d789e2e4d280f713d681bcdba
eddycheong <cheong.e49@gmail.com>
2017-11-13 20:52:51 +0000

    Added rspec authentication for user and edge cases

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before(:all) do
+    User.destroy_all
+    @user = User.new(first_name: "John", last_name: "Doe", password: "john", password_confirmation: "john", email: "john@doe.mia")    
+  end
+  
   describe 'Validations' do
     it 'should create a user when password and password confirmation matches' do
-      @user = User.new(first_name: "John", last_name: "Doe", password: "john", password_confirmation: "john", email: "john@doe.mia")
+      # @user = User.new(first_name: "John", last_name: "Doe", password: "john", password_confirmation: "john", email: "john@doe.mia")
 
       expect(@user).to be_valid
       expect(@user.save).to eq true
     end
 
     it 'should not create a user when password and password confirmation mismatches' do
-      @user = User.new(first_name: "John", last_name: "Doe", password: "john", password_confirmation: "doe", email: "john@doe.mia")
+      # @user = User.new(first_name: "John", last_name: "Doe", password: "john", password_confirmation: "doe", email: "john@doe.mia")
+      @user.password_confirmation = "doe"
 
       expect(@user).to_not be_valid
       expect(@user.save).to eq false
     end
 
     it 'should not create a user when email already exist in the database' do
+      @user1 = User.new(first_name: "John", last_name: "Doe", password: "john", password_confirmation: "john", email: "john@doe.mia")
+      @user2 = User.new(first_name: "Jane", last_name: "Doe", password: "jane", password_confirmation: "jane", email: "john@doe.mia")      
+
+      expect(@user1).to be_valid
+      expect(@user1.save).to eq true
+
+      expect(@user2).to_not be_valid
+      expect(@user2.save).to eq false
+    end
+  
+    it 'should not create a user when email already exist in the database with case insensitivty' do
       @user1 = User.new(first_name: "John", last_name: "Doe", password: "john", password_confirmation: "john", email: "john@doe.mia")
       @user2 = User.new(first_name: "Jane", last_name: "Doe", password: "jane", password_confirmation: "jane", email: "JOHN@doe.mia")      
 
@@ -26,7 +43,7 @@ RSpec.describe User, type: :model do
       expect(@user2).to_not be_valid
       expect(@user2.save).to eq false
     end
-    
+
     it 'should not create a user when first_name is missing' do
       @user = User.new(last_name: "Doe", password: "john", password_confirmation: "john", email: "john@doe.mia")
       
@@ -67,4 +84,32 @@ RSpec.describe User, type: :model do
     end
   end
 
+  describe '.authenticate_with_credentials' do
+    # examples for this class method here
+    before(:all) do
+      User.destroy_all
+      @user = User.create(first_name: "John", last_name: "Doe", password: "john", password_confirmation: "john", email: "john@doe.mia")      
+    end
+
+    it "should return a user when given correct credentials" do
+      @user = User.authenticate_with_credentials("john@doe.mia", "john")
+      expect(@user).to be_a(User)
+    end
+
+    it "should not return a user when given incorrect credentials" do
+      @user = User.authenticate_with_credentials("john@doe.mia", "johnny")
+      expect(@user).to be_nil
+    end
+
+    it "should return a user when given spaces before the email" do
+      @user = User.authenticate_with_credentials("   john@doe.mia", "john")
+      expect(@user).to be_a(User)
+    end
+
+    it "should return a user when given spaces after the email" do
+      @user = User.authenticate_with_credentials("john@doe.mia    ", "john")
+      expect(@user).to be_a(User)
+    end
+
+  end
 end
